adf8c74e9fdf1406377060e9a803407f05c5cf37
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-08-15 15:12:50 -0300

    feat: Alter data for character images

---
+++ src/utils/charactersImages.js
@@ -0,0 +1,43 @@
+import Batman from '../../assets/batman.svg'
+import Robin from '../../assets/robin.svg'
+import Wonder from '../../assets/wonder.svg'
+import Joker from '../../assets/joker.svg'
+import Harry from '../../assets/harry.svg'
+import Superman from '../../assets/superman.svg'
+import Thor from '../../assets/thor.svg'
+import Wolverine from '../../assets/wolverine.svg'
+
+export const charactersImages = (width, height) => [
+  {
+    name: 'joker',
+    image: <Joker width={width} height={height} />,
+  },
+  {
+    name: 'wonder',
+    image: <Wonder width={width} height={height} />,
+  },
+  {
+    name: 'batman',
+    image: <Batman width={width} height={height} />,
+  },
+  {
+    name: 'robin',
+    image: <Robin width={width} height={height} />,
+  },
+  {
+    name: 'harry',
+    image: <Harry width={width} height={height} />,
+  },
+  {
+    name: 'superman',
+    image: <Superman width={width} height={height} />,
+  },
+  {
+    name: 'thor',
+    image: <Thor width={width} height={height} />,
+  },
+  {
+    name: 'wolverine',
+    image: <Wolverine width={width} height={height} />,
+  },
+]
