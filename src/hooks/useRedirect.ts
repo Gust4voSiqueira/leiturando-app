c2998d50c59b052f60576e19ca1b3ca82dfbb495
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-03 21:52:31 -0300

    Removendo arquivo inutil

--- src/hooks/useRedirect.ts
+++
@@ -1,9 +0,0 @@
-import { useLinkTo } from '@react-navigation/native'
-
-export function useRedirect() {
-  const redirect = useLinkTo()
-
-  const onRedirect = (url: string) => redirect(url)
-
-  return { onRedirect }
-}
