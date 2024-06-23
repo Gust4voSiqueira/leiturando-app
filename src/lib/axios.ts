234fb8ac73c2e4ab43e65c1f82397f6a13d26638
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2024-06-23 12:28:57 -0300

    update url axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { AppError } from '../utils/AppError'
 
 const api = axios.create({
-  baseURL: 'http://35.198.13.28:8080',
+  baseURL: 'http://35.215.255.232:8080',
+  timeout: 5000 // 5 segundos
   // baseURL: 'http://localhost:8080',
 })
 
